fb91bcb39d0ed36ae3164ca3d845bd81ac9ec654
Addiowen <owenoscar18@gmail.com>
2024-09-04 10:57:32 +0300

    added spinner

--- src/app/shared/customers/dashboard/requisition-alerts.tsx
+++ src/app/shared/customers/dashboard/requisition-alerts.tsx
@@ -76,7 +76,11 @@ export default function RequisitionAlerts(
     fetchMessages();
   }, []);
 
-  if (loading) return <p>Loading...</p>;
+  if (loading) return (
+    <div className="flex justify-center items-center h-full">
+      <div className="w-12 h-12 border-4 border-t-4 border-gray-200 border-t-blue-500 rounded-full animate-spin"></div>
+    </div>
+  );
 
   return (
     <WidgetCard
--- src/layouts/messages-dropdown.tsx
+++ src/layouts/messages-dropdown.tsx
@@ -1,24 +1,81 @@
 'use client';
 
-import Link from 'next/link';
-import { RefObject, useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
-import { Title, Text, Popover, Avatar, Badge } from 'rizzui';
+import { Title, Text, Badge, Avatar, Popover } from 'rizzui';
 import cn from '@/utils/class-names';
-import { routes } from '@/config/routes';
-import { useMedia } from '@/hooks/use-media';
 import SimpleBar from '@/components/ui/simplebar';
 import { PiCheck } from 'react-icons/pi';
-import { messagesData } from '@/data/messages';
+import { BASE_URL } from '@/lib/axios';
+import Link from 'next/link';
+import { useSession } from 'next-auth/react';
 
 dayjs.extend(relativeTime);
+
+interface Message {
+  id: string;
+  createdDate: string;
+  content: string;
+  read: boolean;
+  senderId: string;
+  receiverId: string;
+}
+
+interface ApiResponse {
+  hasPreviousPage: boolean;
+  hasNextPage: boolean;
+  nbResultsPerPage: number;
+  startCursor: string;
+  endCursor: string;
+  results: Message[];
+}
 
 function MessagesList({
   setIsOpen,
 }: {
   setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    async function fetchMessages() {
+      try {
+        const { data: session } = useSession();
+        if (session) {
+          const userId = session?.user.userId;
+
+          const response = await axios.get<ApiResponse>(
+            `${BASE_URL}/messages?page=1&nbResultsPerPage=10&orderBy=createdDate&order=desc&receiverId=${userId}`,
+            {
+              headers: {
+                Authorization: 'Basic c2Vja190ZXN0X3dha1dBNDFyQlRVWHMxWTVvTlJqZVk1bzo=',
+              },
+            }
+          );
+
+          setMessages(response.data.results);
+        } else {
+          console.error('No session data found in session storage.');
+        }
+      } catch (error) {
+        console.error('Error fetching messages:', error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchMessages();
+  }, []);
+
+  if (loading) return (
+    <div className="flex justify-center items-center h-full">
+      <div className="spinner"></div>
+    </div>
+  );
+
   return (
     <div className="w-[320px] text-left sm:w-[360px] 2xl:w-[420px] rtl:text-right">
       <div className="mb-2 flex items-center justify-between ps-6">
@@ -26,7 +83,7 @@ function MessagesList({
           Messages
         </Title>
         <Link
-          href={routes.support.inbox}
+          href="/messages" // Update the link as needed
           onClick={() => setIsOpen(false)}
           className="hover:underline"
         >
@@ -35,54 +92,44 @@ function MessagesList({
       </div>
       <SimpleBar className="max-h-[406px]">
         <div className="grid grid-cols-1 ps-4">
-          {messagesData.map((item) => (
+          {messages.map((message) => (
             <div
-              key={item.name + item.id}
+              key={message.id}
               className="group grid cursor-pointer grid-cols-[auto_minmax(0,1fr)] gap-2.5 rounded-md px-2 py-2.5 pe-3 transition-colors hover:bg-gray-100 dark:hover:bg-gray-50"
             >
-              <div className={cn('relative', item.avatar.length > 1 && 'me-1')}>
+              <div className="relative">
                 <Avatar
-                  src={item.avatar[0]}
-                  name={item.name}
-                  className={cn(
-                    item.avatar.length > 1 &&
-                      'relative -end-1 -top-0.5 !h-9 !w-9'
-                  )}
+                  src={message.senderId} // You may need to adjust this based on how avatars are handled
+                  name={message.senderId}
+                  className="h-9 w-9"
                 />
-                {item.avatar.length > 1 && (
-                  <Avatar
-                    src={item.avatar[1]}
-                    name={item.name}
-                    className="absolute -bottom-1 end-1.5 !h-9 !w-9 border-2 border-gray-0 dark:border-gray-100"
-                  />
-                )}
               </div>
               <div className="grid grid-cols-[minmax(0,1fr)_auto] items-center">
                 <div className="w-full">
                   <Text className="mb-0.5 w-11/12 truncate text-sm font-semibold text-gray-900 dark:text-gray-700">
-                    {item.name}
+                    {message.senderId}
                   </Text>
                   <div className="flex">
                     <Text className="w-10/12 truncate pe-7 text-xs text-gray-500">
-                      {item.message}
+                      {message.content}
                     </Text>
                     <Text className="ms-auto whitespace-nowrap pe-8 text-xs text-gray-500">
-                      {dayjs(item.sendTime).fromNow(true)}
+                      {dayjs(message.createdDate).fromNow(true)}
                     </Text>
                   </div>
                 </div>
                 <div className="ms-auto flex-shrink-0">
-                  {item.unRead ? (
+                  {message.read ? (
+                    <span className="inline-block rounded-full bg-gray-100 p-0.5 dark:bg-gray-50">
+                      <PiCheck className="h-auto w-[9px]" />
+                    </span>
+                  ) : (
                     <Badge
                       renderAsDot
                       size="lg"
                       color="primary"
                       className="scale-90"
                     />
-                  ) : (
-                    <span className="inline-block rounded-full bg-gray-100 p-0.5 dark:bg-gray-50">
-                      <PiCheck className="h-auto w-[9px]" />
-                    </span>
                   )}
                 </div>
               </div>
@@ -97,16 +144,15 @@ function MessagesList({
 export default function MessagesDropdown({
   children,
 }: {
-  children: JSX.Element & { ref?: RefObject<any> };
+  children: JSX.Element & { ref?: React.RefObject<any> };
 }) {
-  const isMobile = useMedia('(max-width: 480px)', false);
   const [isOpen, setIsOpen] = useState(false);
   return (
     <Popover
       isOpen={isOpen}
       setIsOpen={setIsOpen}
       shadow="sm"
-      placement={isMobile ? 'bottom' : 'bottom-end'}
+      placement="bottom-end"
     >
       <Popover.Trigger>{children}</Popover.Trigger>
       <Popover.Content className="z-[9999] pb-6 pe-6 ps-0 pt-5 dark:bg-gray-100 [&>svg]:hidden [&>svg]:dark:fill-gray-100 sm:[&>svg]:inline-flex">
