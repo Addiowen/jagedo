d8e3a33d8331547249628fe82e86767a3952ce12
itsmraga-hub <itsragamit@gmail.com>
2024-09-11 20:05:44 +0300

    Update Footer styling

--- src/app/home/components/Footer.tsx
+++ src/app/home/components/Footer.tsx
@@ -1,6 +1,15 @@
 import * as React from 'react';
 import Image from 'next/image';
 import Copyright from '@public/icons/copyright.png';
+import Email from '@public/icons/Email.png';
+import Slice1 from '@public/icons/Slice 2.png';
+import Slice from '@public/icons/Slice 1.png';
+import Calling from '@public/icons/Calling.png';
+import playstore from '@public/icons/google.png';
+import appstore from '@public/icons/appstore.jpg';
+
+
+
 import '../main.css';
 
 
@@ -13,10 +22,15 @@ const LandingFooter: React.FC = () => {
                     <a href=''>Terms of Use</a> - 
                     <a href=''> Privacy Policy</a>
                 </span>
+                <div style={{display: 'flex', gap: '1rem'}}>
+                <Image src={playstore} width={100} alt="Isomorphic" className="copyright" />
+                <Image src={appstore} width={100} alt="Isomorphic" className="copyright" />
+                </div>
             </div>
             <div>
-                <p>+254716108282</p>
-                <a href="mailto:info@jagedo.co.ke">info@jagedo.co.ke</a>
+                <p>
+                    <Image src={Calling} width={15} alt="Isomorphic" className="copyright" />   +254716108282</p>
+                <Image src={Email} width={15} alt="Isomorphic" className="copyright" /><a href="mailto:info@jagedo.co.ke">  info@jagedo.co.ke</a>
                 <div>
 
                 </div>
