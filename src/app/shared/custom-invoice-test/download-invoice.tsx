f64bae213c24580b366f0f48f213f986f50780b6
Addiowen <owenoscar18@gmail.com>
2024-09-26 15:12:56 +0300

    fixed invoice download

--- src/app/shared/custom-invoice-test/download-invoice.tsx
+++ src/app/shared/custom-invoice-test/download-invoice.tsx
@@ -15,16 +15,17 @@ import { routes } from '@/config/routes';
 import axios, { BASE_URL } from '@/lib/axios';
 
 // Explicitly type the functional component
-const InvoiceComponent: React.FC = () => {
+const DownloadInvoiceComponent: React.FC = () => {
   const componentRef = useRef<HTMLDivElement>(null);
   const [requestDetails, setRequestDetails] = useState<RequestDetails | null>(
     null
   );
   const [loading, setLoading] = useState(false); // Loading state for data fetch
   const [paymentLoading, setPaymentLoading] = useState(false); // Loading state for payment
-  const [paymentStatus, setPaymentStatus] = useState('Unpaid');
   const [phoneNumber, setPhoneNumber] = useState('');
   const [isPhoneNumberValid, setIsPhoneNumberValid] = useState(true);
+
+  const handleBack = () => router.back();
 
   const { data: session } = useSession();
   const userPhone = session?.user.metadata.phone;
@@ -54,6 +55,10 @@ const InvoiceComponent: React.FC = () => {
 
     fetchData();
   }, [transactionId]);
+
+  const transactionStatus = requestDetails?.status;
+  const [paymentStatus, setPaymentStatus] = useState(transactionStatus === 'draft' ? 'Unpaid' : 'Paid');
+  // console.log(requestDetails);
 
   const requestType = requestDetails?.metadata.packageType;
   const managed = requestDetails?.metadata.managed;
@@ -128,60 +133,6 @@ const InvoiceComponent: React.FC = () => {
     setIsPhoneNumberValid(/^\d{10}$/.test(value)); // Validate that the phone number is 10 digits
   };
 
-  const handlePayment = async () => {
-    if (!isPhoneNumberValid) {
-      toast.error('Please enter a valid 10-digit phone number.');
-      return;
-    }
-
-    setPaymentLoading(true); // Set loading to true when payment starts
-
-    try {
-      const mpesaResponse = await axios.post(
-        `${process.env.NEXT_PUBLIC_DOMAIN}/stkpush`,
-        {
-          phoneNumber: `254${phoneNumber.slice(1)}`, // Convert to international format
-          amount: linkageFee,
-          accountReference: transactionId,
-        }
-      );
-
-      const { success, message, data } = mpesaResponse.data;
-
-      if (success) {
-        toast.success('Payment Successful');
-        const res = await axios.patch(
-          `${BASE_URL}/transactions/${transactionId}`,
-          paidRequest,
-          {
-            headers: {
-              Authorization: process.env.NEXT_PUBLIC_SECRET_AUTH_TOKEN,
-            },
-          }
-        );
-
-        const transactionDetails = res.data;
-        console.log(transactionDetails, 'transactionDetails');
-
-        if (transactionDetails) {
-          toast.success(<Text as="b">Transaction Completed Successfully</Text>);
-          router.refresh();
-          setPaymentStatus('Paid');
-          router.push(
-            `${routes.customers.requisitions}?transactionId=${transactionId}`
-          );
-        }
-      } else {
-        toast.error(data.ResultDesc || message);
-      }
-    } catch (error) {
-      console.log(error);
-      toast.error('Payment Failed. Please try again.');
-    } finally {
-      setPaymentLoading(false); // Set loading to false when payment completes
-    }
-  };
-
   // Function to handle printing
   const handlePrint = useReactToPrint({
     content: () => componentRef.current,
@@ -217,7 +168,9 @@ const InvoiceComponent: React.FC = () => {
 
   return (
     <div className="p-6">
-      {/* <div className=" flex items-center justify-end gap-3 px-8 pt-2 @lg:mt-0">
+      <div className=" flex items-center justify-end gap-3 px-8 pt-2 @lg:mt-0">
+        {' '}
+        {/* Added px-4 for padding */}
         <PrintButton
           onClick={handlePrint}
           className="mb-4 w-auto p-2 text-sm @lg:w-auto"
@@ -232,7 +185,7 @@ const InvoiceComponent: React.FC = () => {
           <FaDownload className="me-1.5 h-[12px] w-[12px]" />
           Download
         </Button>
-      </div> */}
+      </div>
       <div
         className="w-full rounded-xl border border-muted p-3 text-xs"
         ref={componentRef}
@@ -323,64 +276,13 @@ const InvoiceComponent: React.FC = () => {
 
         {/* Actions (hidden during print/download) */}
       </div>
-      <div className="flex flex-col mt-4 xs:flex-row xs:items-center justify-center xs:gap-2">
-        <div className="mb-1 xs:mb-0">
-          <Input
-            type="text"
-            placeholder="Phone Number"
-            value={phoneNumber}
-            onChange={handlePhoneNumberChange}
-            className={`border ${!isPhoneNumberValid ? 'border-red-500' : ''}`}
-          />
-        </div>
-
-        <Button
-          className="w-full xs:w-auto"
-          onClick={handlePayment}
-          disabled={paymentLoading} // Disable button when payment is loading
-        >
-          {paymentLoading ? (
-            <span className="flex items-center">
-              <svg
-                className="mr-3 h-8 w-8 animate-spin text-blue-500"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-              >
-                <circle
-                  cx="12"
-                  cy="12"
-                  r="10"
-                  stroke="currentColor"
-                  strokeWidth="4"
-                  strokeLinecap="round"
-                  className="text-blue-500"
-                  fill="none"
-                />
-                <circle
-                  cx="12"
-                  cy="12"
-                  r="10"
-                  stroke="currentColor"
-                  strokeWidth="4"
-                  strokeLinecap="round"
-                  strokeDasharray="63"
-                  strokeDashoffset="50"
-                  className="text-gray-200"
-                  fill="none"
-                  transform="rotate(-90 12 12)"
-                />
-              </svg>
-              <span>Processing...</span>
-            </span>
-          ) : (
-            'Pay'
-          )}
+      <div className="flex justify-center">
+        <Button className="px-8 mt-4" onClick={handleBack} >
+          Back
         </Button>
       </div>
     </div>
   );
 };
 
-export default InvoiceComponent;
+export default DownloadInvoiceComponent;
