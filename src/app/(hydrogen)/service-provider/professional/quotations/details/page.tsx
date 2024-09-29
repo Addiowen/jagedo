982cb2a27a54b3dea2cb9b9281d0027a612de46e
itsmraga-hub <itsragamit@gmail.com>
2024-09-29 20:49:59 +0300

    Fix quotations redirection page

--- src/app/(hydrogen)/service-provider/professional/quotations/details/page.tsx
+++ src/app/(hydrogen)/service-provider/professional/quotations/details/page.tsx
@@ -1,5 +1,6 @@
 import ViewProfessionalQuotationComponent from '@/app/shared/service-provider/view-quotation/professional';
 import { metaObject } from '@/config/site.config';
+import apiRequest from '@/lib/apiService';
 import { Title } from 'rizzui';
 // import { Title } from 'rizzui';
 
@@ -7,13 +8,32 @@ export const metadata = {
   ...metaObject(),
 };
 
-export default function QuotationDetailsPage() {
+
+export default async function QuotationDetailsPage({
+  searchParams,
+}: {
+  searchParams?: any;
+}) {
+  const fetchRequestDetails = async () => {
+    try {
+      const transactionDetails = await apiRequest({
+        method: 'GET',
+        endpoint: `/transactions/${searchParams.id}`,
+      });
+  
+      return transactionDetails;
+    } catch (error) {
+      console.error('Error fetching user details:', error);
+    }
+  };
+  
+  const requestDetails = await fetchRequestDetails();
   return (
     <>
       <Title as="h4" className="mb-2 pb-5 font-semibold @2xl:mb-5">
         QTN0021
       </Title>
-      <ViewProfessionalQuotationComponent />
+      <ViewProfessionalQuotationComponent quotationDetails={requestDetails}  />
     </>
   );
 }
