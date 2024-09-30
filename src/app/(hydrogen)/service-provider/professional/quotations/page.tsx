2f01930b7ae696169df170d5483eb0fe351f3383
itsmraga-hub <itsragamit@gmail.com>
2024-09-30 23:42:01 +0300

    Filter transactions based on quotations

--- src/app/(hydrogen)/service-provider/professional/quotations/page.tsx
+++ src/app/(hydrogen)/service-provider/professional/quotations/page.tsx
@@ -3,10 +3,12 @@
 // import ProfessionalQuotationsTable from '@/app/shared/service-provider/tables/sp-quotations-table';
 // import ContractorQuotationsTable from '@/app/shared/service-provider/tables/sp-quotations-table/contractor';
 // import ContractorRequisitionsTable from '@/app/shared/service-provider/tables/sp-requisitions-table/contractor';
+import { authOptions } from '@/app/api/auth/[...nextauth]/auth-options';
 import ProfessionalQuotationsTable from '@/app/shared/service-provider/tables/sp-quotations-table/professional';
 import { metaObject } from '@/config/site.config';
 import { BASE_URL } from '@/lib/axios';
 import axios from 'axios';
+import { getServerSession } from 'next-auth';
 import { Title } from 'rizzui';
 
 export const metadata = {
@@ -28,8 +30,18 @@ const fetchQuotations = async (params: any) => {
 
 export default async function QuotationsPage() {
 
+  const session = await getServerSession(authOptions);
+
+
+  const userId = session?.user?.metadata?.assetId;
+
   const quotationData = await fetchQuotations({});
   const quotations = await quotationData.data.results;
+
+  const filteredQuotations = quotations.filter((quotation: { metadata: { professionals: any[]; }; }) => {
+    return quotation.metadata?.professionals?.includes(session?.user?.metadata?.assetId);
+  });
+
   console.log(quotations, 'quotationData');
   return (
     <>
@@ -39,7 +51,7 @@ export default async function QuotationsPage() {
 
       <div className="@container">
         <div className="grid grid-cols-1 gap-6 @4xl:grid-cols-1 3xl:gap-8">
-          <ProfessionalQuotationsTable quotationData={quotations}  className="relative @4xl:col-span-12  @7xl:col-span-8" />
+          <ProfessionalQuotationsTable quotationData={filteredQuotations}  className="relative @4xl:col-span-12  @7xl:col-span-8" />
         </div>
       </div>
     </>
